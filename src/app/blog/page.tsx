8196af3e6b0c299c8674d2ebeb1a04cf27ee0e57
Charan-vamshi <saicharanreddy344@gmail.com>
2026-02-03 02:21:47 +0530

    Add blog coming soon page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+export default function BlogPage() {
+  return (
+    <section style={{ padding: "80px 40px", textAlign: "center" }}>
+      <h1 className="glitch" data-text="Blog">
+        Blog
+      </h1>
+
+      <p style={{ marginTop: "25px", color: "#777" }}>
+        Coming Soon. This section will contain AI + engineering writeups.
+      </p>
+    </section>
+  );
+}
